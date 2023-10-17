9e3a94156b6c54ad0d5d552c41807ca509784e7b
cecidlp <delaparra.cecilia@gmail.com>
2023-10-17 14:34:32 +0200

    create note usestate for storing title and details

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {IoIosArrowBack} from 'react-icons/io'
+import { IoIosArrowBack } from 'react-icons/io'
+import { useState } from 'react'
+import {v4 as uuid} from 'uuid'
 
 const CreateNote = () => {
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(title && details){
+      const note = {id: uuid(), title, details}
+
+    }
+  }
+
   return (
     <section>
       <header className="create-note__header">
-        <Link to='/' className='btn'><IoIosArrowBack/> </Link>
-        <button className="btn lg primary">Save</button>
+        <Link to='/' className='btn'><IoIosArrowBack /> </Link>
+        <button className="btn lg primary" onClick={handleSubmit} >Save</button>
       </header>
-      <form action="" className='create-note__form'>
-        <input type="text" placeholder='Title' autoFocus />
-        <textarea rows="28" placeholder='Note details...'></textarea>
+      <form action="" className='create-note__form' onSubmit={handleSubmit} >
+        <input type="text" placeholder='Title' onChange={(e) => setTitle(e.target.value)} value={title} autoFocus />
+        <textarea rows="28" placeholder='Note details...' onChange={(e)=> setDetails(e.target.value)} value={details} ></textarea>
       </form>
     </section>
   )
--- src/router/RouterPage.jsx
+++ src/router/RouterPage.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom"
 import Notes from '../pages/Notes'
 import CreateNote from '../pages/CreateNote'
 import EditNotes from '../pages/EditNotes'
+import dummyNotes from '../dummy_notes'
+import { useState } from "react";
 
 const RouterPage = () => {
+    const [notes, setNotes] = useState(dummyNotes)
+
     return (
         <div id="app">
             <Routes>
-                <Route path="/" element={<Notes/>} />
+                <Route path="/" element={<Notes notes={notes} />} />
                 <Route path="/create-note" element={<CreateNote/>} />
                 <Route path="/edit-note/:id" element={<EditNotes/>} />
             </Routes>
